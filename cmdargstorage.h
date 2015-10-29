57f631ce828342ed0a905a06e46f633219c3256d
Robert H <robert.w.hoath@student.shu.ac.uk>
2015-10-29 21:26:00 +0000

    Build fixes

--- cmdargstorage.h
+++ cmdargstorage.h
@@ -16,11 +16,11 @@ typedef std::function<bool(const std::string &)> t_callback;
 
 template <class T> class storage : public storagebase<T> {
 public:
-  storage(T default_val) : storagebase(default_val) {}
+  storage(T default_val) : storagebase<T>(default_val) {}
 
   virtual bool on_set(const std::string &raw) {
-    touched_ = parse_storage(raw, storage_);
-    return touched_;
+    this->touched_ = parse_storage(raw, this->storage_);
+    return this->touched_;
   }
 };
 
@@ -31,8 +31,8 @@ public:
   virtual bool on_set(const std::string &raw) {
     // Minor variant here. Callbacks are always touched, the success depends on
     // the function.
-    touched_ = true;
-    return storage_(raw);
+    this->touched_ = true;
+    return this->storage_(raw);
   }
 };
 
@@ -41,8 +41,8 @@ public:
   virtual bool on_set(const std::string &raw) {
     T tmp;
     if (parse_storage(raw, tmp)) {
-      touched_ = true;
-      storage_.push_back(tmp);
+      this->touched_ = true;
+      this->storage_.push_back(tmp);
       return true;
     }
 
